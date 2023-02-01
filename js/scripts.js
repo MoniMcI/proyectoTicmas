ab3b1092e10d1035021533cd5dc6951271744b33
MoniMcI <123341063+MoniMcI@users.noreply.github.com>
2023-01-31 23:57:18 -0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+function mostrarEmail() {
+  document.getElementById("texto").innerHTML = "Mi email es mariarosas@mail.com";
+}
+
+function mostrarTelefono() {
+  document.getElementById("texto").innerHTML = "Mi teléfono es +54 123 456 7890";
+}
+
+function mostrarDomicilio() {
+  document.getElementById("texto").innerHTML = "Mi domicilio es Salta 4400";
+}
+
+function mostrarFechaNac() {
+  document.getElementById("texto").innerHTML = "Mi fecha de nacimiento es 03/10/1987";
+}
